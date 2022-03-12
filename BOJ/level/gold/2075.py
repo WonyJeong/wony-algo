9cedb1552a067c11db3cbdfc4391182a71c3b7bc
정원영 <jwy27772415@gmail.com>
2022-03-12 13:08:52 +0900

    Create 2075.py

---
+++ BOJ/level/gold/2075.py
@@ -0,0 +1,19 @@
+import heapq
+import sys
+input = sys.stdin.readline
+
+def solution(N, arr):
+    q = []
+    for i in range(N):
+        for j in range(N):
+            k = arr[i][j]
+            if len(q)
+            heapq.heappush(q, (-k, k))
+    print(q, len(q))
+
+if __name__ == '__main__':
+    N = int(input().strip())
+    arr = []
+    for _ in range(N):
+        arr.append(list(map(int, input().strip().split())))
+    solution(N, arr)
